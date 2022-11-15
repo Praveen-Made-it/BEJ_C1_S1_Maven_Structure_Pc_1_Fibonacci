c98806f2b197a15e172558ec0738300cb2550ef2
Praveen Kumar <praveen170596@gmail.com>
2022-11-15 20:52:24 +0530

    code completed for Fibonacci series

--- src/main/java/com/niit/jdp/Fibonacci/Fibonacci.java
+++ src/main/java/com/niit/jdp/Fibonacci/Fibonacci.java
@@ -8,4 +8,20 @@
 package com.niit.jdp.Fibonacci;
 
 public class Fibonacci {
+    public int[] fibonacci(int number) {
+        int number1 = 0;
+        int number2 = 1;
+        int number3;
+        int fibonacciSeries[] = new int[number];
+        fibonacciSeries[0] = 0;
+        fibonacciSeries[1] = 1;
+
+        for (int i = 0; i < number - 2; i++) {
+            number3 = number1 + number2;
+            fibonacciSeries[i + 2] = number3;
+            number1 = number2;
+            number2 = number3;
+        }
+        return fibonacciSeries;
+    }
 }
